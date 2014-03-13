9dd614006b64fc66637e8946660c03b1dc0b0939
mironovaOlga <olga.mironova@db.com>
2014-03-13 12:04:42 +0400

    test runner

---
+++ practice_3/test_runner.py
@@ -0,0 +1,19 @@
+def fn():
+    pass
+
+def test_runner():
+    test = {}
+    def add_test(fn):
+        test[fn.__name__] = []
+    def pending_tests():
+        
+    def run():
+        
+    def ran_tests():
+        pass
+    def passed_tests():
+        pass
+    def failed_tests():
+        pass
+    def clear_state():
+        pass
